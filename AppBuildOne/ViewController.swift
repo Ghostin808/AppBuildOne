c493fae6156c72748a29fbaacee165d81e69f3df
Joseph Payongayong <Jp47@hawaii.edu>
2020-02-18 10:47:32 -1000

    Still working on part 5 of VC1

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var aboutUhwoButtonPressed: UIButton!
     @IBOutlet weak var uhwo: UILabel!
+    
+    @IBOutlet weak var FirstTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.uhwo.text = "University of Hawaiʻi West Oʻahu"
         self.view.backgroundColor = UIColor.gray
-        
+        self.FirstTextView.text = "UH West Oʻahu has been the fastest growing school of its type in the nation for the past two years, according to the Chronicle of Higher Education’s annual almanac. Enrollment surged 239 percent between 2006 and 2016, or the most by any public baccalaureate granting institution. In 2017 UH West Oʻahu led the list of fastest growing colleges."
         
     }
-
+    @IBAction func AboutACMbuttonpressed(_ sender: Any) {
+    self.uhwo.text = "About ACM"
+    self.FirstTextView.text = "Students who chose to study creative media will receive an innovative educational experience that weaves art and design, narrative, information science, and technologies necessary for a broad range of careers in government and non-government organizations, educational institutions, technological firms and private firms and agencies."
+    }
+    
 }
 
